6e3a6fb6928a4c5035e4bb100b8ec7e00679ec1c
Gauthier Peyre <g.p220498@gmail.com>
2024-07-18 15:24:21 +0200

    toujours pas de mariadb fonctionnel

--- srcs/requirements/mariadb/tools/create_user.sh
+++ srcs/requirements/mariadb/tools/create_user.sh
@@ -3,6 +3,6 @@ service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};"
 mysql -e "CREATE USER IF NOT EXISTS ${WORDPRESS_DB_USER}@'localhost' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO ${WORDPRESS_DB_USER}@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p'no' shutdown
+mysqladmin -u root -p'${MYSQL_ROOT_PASSWORD}' shutdown
